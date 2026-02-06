d3b09a4ef768d0a8171d3c423eabe3b448d3f3c7
albuqerques <rchard.asc@gmail.com>
2026-02-06 12:15:59 -0300

    mini projeto2

---
+++ mini-projeto2/utilitarios/exceptions.py
@@ -0,0 +1,12 @@
+class SaldoInsuficienteError(Exception):
+    def __init__(self, saldo_atual, valor_saque, mensagem="Saldo insuficiente para realizar o saque."):
+        self.saldo_atual = saldo_atual
+        self.valor_saque = valor_saque
+        self.mensagem = f"{mensagem} Saldo atual: R${saldo_atual:.2f}, Tentativa de saque: R${valor_saque:.2f}"
+        super().__init__(self.mensagem)
+
+class ContaInexistenteError(Exception):
+    def __init__(self, numero_conta, mensagem="A conta especificada não foi encontrada."):
+        self.numero_conta = numero_conta
+        self.mensagem = f"{mensagem} Número da conta: {numero_conta}"
+        super().__init__(self.mensagem)
